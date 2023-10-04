0d1e891d6ad4e897fec947e55ed30a395ab7c283
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-04 11:03:51 +0200

    Customer Service Initialization

---
+++ src/test/java/com/dgmf/customer/service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.customer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
