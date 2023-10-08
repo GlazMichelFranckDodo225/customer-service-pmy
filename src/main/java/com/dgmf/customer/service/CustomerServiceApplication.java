3fc2c6f2a8bfb024aae6d54e76448de7df313ad9
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-08 07:05:54 +0200

    Additions and Retrievals of 5 Customers

--- src/main/java/com/dgmf/customer/service/CustomerServiceApplication.java
+++ src/main/java/com/dgmf/customer/service/CustomerServiceApplication.java
@@ -1,7 +1,13 @@
 package com.dgmf.customer.service;
 
+import com.dgmf.customer.service.entity.Customer;
+import com.dgmf.customer.service.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -10,4 +16,41 @@ public class CustomerServiceApplication {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
+	// Executed at Application startup
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository) {
+		return args -> {
+			// Saving 5 Customers
+			customerRepository.saveAll(List.of(
+					Customer.builder()
+							.firstName("John")
+							.lastName("Doe")
+							.email("johndoe@gmail.com")
+							.build(),
+					Customer.builder()
+							.firstName("Mary")
+							.lastName("Grace")
+							.email("marygrace@gmail.com")
+							.build(),
+					Customer.builder()
+							.firstName("Jean")
+							.lastName("Rivery")
+							.email("jeanrivery@gmail.com")
+							.build(),
+					Customer.builder()
+							.firstName("Nolan")
+							.lastName("Hombredo")
+							.email("nolanhombredo@gmail.com")
+							.build(),
+					Customer.builder()
+							.firstName("Fatou")
+							.lastName("Cissé")
+							.email("fatoucisse@gmail.com")
+							.build()
+			));
+
+			// Fetching and Displaying all the Customers
+			customerRepository.findAll().forEach(System.out::println);
+		};
+	}
 }
--- src/main/java/com/dgmf/customer/service/repository/CustomerRepository.java
+++ src/main/java/com/dgmf/customer/service/repository/CustomerRepository.java
@@ -4,7 +4,8 @@ import com.dgmf.customer.service.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-// To expose Restfull Web Service that handles Customers
+// Allows Spring Data Rest to expose Restfull Web Services that
+// handle Customers on the Endpoint ==> http://localhost:8081/customers
 @RepositoryRestResource
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
